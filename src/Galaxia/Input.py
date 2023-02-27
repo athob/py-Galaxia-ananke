75f5661c48156d20683524b5afa5f37f9a917b06
Adrien Thob <adrien.thob@gmail.com>
2023-02-27 14:21:14 -0500

    Implemented the possibility to give pre-generated Galaxia compatible input files to the Input object

--- src/Galaxia/Input.py
+++ src/Galaxia/Input.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 """
-Docstring
+Contains the Input class definition
+
+Please note that this module is private. The Input class is
+available in the main ``Galaxia`` namespace - use that instead.
 """
+import re
 import pathlib
 import numpy as np
 import ebf
@@ -20,16 +24,42 @@ class Input:
     _density = 'density'
     _required_keys_in_particles = {_pos, _vel, _mass, 'age', 'parentid', 'dform', 'feh', 'alpha', 'helium', 'carbon', 'nitrogen', 'oxygen', 'neon', 'magnesium', 'silicon', 'sulphur', 'calcium'}
     _optional_keys_in_particles = {'id'}
-    def __init__(self, particles, rho_pos, rho_vel=None, name='sim', knorm=0.596831, ngb=64) -> None:
-        self.__particles = particles
+    def __init__(self, *args, **kwargs) -> None:
+        if args:
+            if len(args) != 2: raise  # TODO mix & match args & kwargs for particles and rho_pos
+            self.__input_dir = GALAXIA_TMP
+            kwargs['particles'] = args[0]
+            kwargs['rho_pos'] = args[1]
+            self.__input_files_exist = False
+        elif {'pname', 'kname'}.issubset(kwargs.keys()):  # TODO implement case where pname and kname non-formated names
+            _pname = pathlib.Path(kwargs['pname'])
+            _kname = pathlib.Path(kwargs['kname'])
+            if _pname.parent != _kname.parent: raise  # TODO write error message
+            self.__input_dir = _pname.parent
+            kwargs['pname'] = _pname.name
+            kwargs['kname'] = _kname.name
+            kwargs['name'] = re.findall("(.*).ebf", kwargs['pname'])[0]
+            _hdim, kwargs['ngb'] = map(int, re.findall(f"{kwargs['name']}_d(\d*)n(\d*)_den.ebf",
+                                                       kwargs['kname'])[0])  # TODO what if _hdim is 3 ?
+            kwargs['particles'] = ebf.read(_pname)
+            _k =  ebf.read(_kname)
+            _mass = _k[self._mass]  # dummy line to check format
+            kwargs['rho_pos'] = _k[self._density]
+            _knorm = _k['h_cubic'][:,0] * np.cbrt(_k['density']) / np.sqrt(kwargs['ngb'])
+            kwargs['knorm'] = np.median(_knorm) if len(np.unique(np.round(_knorm/(2*np.finfo(_knorm.dtype).eps)).astype('int')))==1 else _knorm
+            kwargs['rho_vel'] = (np.sqrt(kwargs['ngb']) * kwargs['knorm'] / _k[self._kernels][:,1])**3
+            self.__input_files_exist = True
+        else:
+            raise  # TODO write error message
+        self.__particles = kwargs['particles']
         self.__verify_particles()
-        self.__rho_pos = rho_pos
-        self.__rho_vel = rho_vel
-        self.__name = name
-        self.__pname = None
-        self.__kname = None
-        self.__knorm = knorm
-        self.__ngb = ngb
+        self.__rho_pos = kwargs['rho_pos']
+        self.__rho_vel = kwargs.get('rho_vel')
+        self.__name = kwargs.get('name', 'sim')
+        self.__pname = kwargs.get('pname', None)
+        self.__kname = kwargs.get('kname', None)
+        self.__knorm = kwargs.get('knorm', 0.596831)
+        self.__ngb = kwargs.get('ngb', 64)
 
     @property
     def particles(self):
@@ -68,19 +98,23 @@ class Input:
         return self.__ngb
 
     @property
+    def _input_dir(self):
+        return self.__input_dir
+
+    @property
     def kernels(self):
         return np.sqrt(self.ngb) * self.knorm / np.cbrt(self.rho)
     
     @property
     def kname(self):
         if self.__kname is None:
-            self.__kname = GALAXIA_TMP / f"{self.name}_d{self.hdim}n{self.ngb}_den.ebf"
+            self.__kname = self._input_dir / f"{self.name}_d{self.hdim}n{self.ngb}_den.ebf"
         return self.__kname
 
     @property
     def pname(self):
         if self.__pname is None:
-            self.__pname = GALAXIA_TMP / f"{self.name}.ebf"
+            self.__pname = self._input_dir / f"{self.name}.ebf"
         return self.__pname
 
     def __verify_particles(self):
@@ -105,25 +139,27 @@ class Input:
         temp_filename.write_text(FILENAME_TEMPLATE.substitute(name=self.name, pname=pname.name))
         parfile = pathlib.Path(kwargs.pop('parfile', 'survey_params'))  # TODO make temporary? create a global record of temporary files?
         if not parfile.is_absolute():
-            parfile = GALAXIA_TMP / parfile
+            parfile = self._input_dir / parfile
         parfile.write_text(PARFILE_TEMPLATE.substitute(DEFAULTS_FOR_PARFILE, photo_categ=isochrone.category, photo_sys=isochrone.name, mag_color_names=cmd_magnames, nres=self.ngb, **kwargs))  # output_file=surveyname, fsample=fsample))
         return self.name, parfile
 
     def _write_kernels(self):
         kname = self.kname
-        ebf.initialize(self.kname)
-        ebf.write(kname, f"/{self._density}", self.rho_pos, "a")
-        ebf.write(kname, f"/{self._kernels}", self.kernels, "a")
-        ebf.write(kname, f"/{self._mass}", self.particles['mass'], "a")
+        if not self.__input_files_exist:
+            ebf.initialize(self.kname)
+            ebf.write(kname, f"/{self._density}", self.rho_pos, "a")
+            ebf.write(kname, f"/{self._kernels}", self.kernels, "a")
+            ebf.write(kname, f"/{self._mass}", self.particles['mass'], "a")
         return kname
 
     def _write_particles(self):
         pname = self.pname
-        ebf.initialize(pname)
-        for key in self._required_keys_in_particles:
-            ebf.write(pname, f"/{key}", self.particles[key], 'a')
-        for key in self._optional_keys_in_particles:
-            ebf.write(pname, f"/{key}", self.particles[key] if key in self.particles.keys() else np.zeros(self.length), 'a')
+        if not self.__input_files_exist:
+            ebf.initialize(pname)
+            for key in self._required_keys_in_particles:
+                ebf.write(pname, f"/{key}", self.particles[key], 'a')
+            for key in self._optional_keys_in_particles:
+                ebf.write(pname, f"/{key}", self.particles[key] if key in self.particles.keys() else np.zeros(self.length), 'a')
         return pname
 
 
