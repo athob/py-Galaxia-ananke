689efe0608f5fbea5bc5b590154e41759074d8e4
Adrien Thob <adrien.thob@gmail.com>
2024-05-17 13:48:23 -0400

    Include full license text in metadata

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ from src.__metadata__ import *
 
 ROOT_DIR = pathlib.Path(__file__).parent
 
+for_all_files = ('__license__', )
+
 long_description = ""
+
+package_data = {NAME: all_files(*for_all_files,
+                                basedir=pathlib.Path(SRC_DIR, NAME))}
 
 
 class MyBuildExt(build_ext):
@@ -44,6 +49,8 @@ setup(name=NAME,
       python_requires='>=3.8,<3.11',
       packages=[NAME, f"{NAME}.photometry"],
       package_dir={'': SRC_DIR},
+      package_data=package_data,
+      include_package_data=True,
       install_requires=['numpy>=1.22,<2', 'pandas>=2,<3', 'vaex>=4.17,<5', 'astropy>=5,<7', 'h5py>=3.6,<4', 'ebfpy>=0.0.20,<1', 'astroquery>=0.4.2,<1'],
       ext_modules=[setuptools.extension.Extension('', [])],
       cmdclass={'build_ext': MyBuildExt, 'test': MyTest},
